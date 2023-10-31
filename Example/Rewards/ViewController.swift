33d3587855eb7b71890fbc37109835d79abd7b88
Jessé Monteiro <jessemonteiroferreira@gmail.com>
2023-10-31 15:42:58 -0300

    Initial commit

---
+++ Example/Rewards/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Rewards
+//
+//  Created by 8730443 on 10/31/2023.
+//  Copyright (c) 2023 8730443. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+

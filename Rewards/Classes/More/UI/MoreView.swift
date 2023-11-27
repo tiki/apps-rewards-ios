1ec1d450471d2abb157b31522f13a60f69e884ec
Ricardo Gonçalves <ricardobrg@users.noreply.github.com>
2023-11-27 16:49:22 -0300

    fix: text with wrong pasting

--- Rewards/Classes/More/UI/MoreView.swift
+++ Rewards/Classes/More/UI/MoreView.swift
@@ -28,7 +28,7 @@ public struct MoreView: View{
                     .padding(.top, 34)
                 MoreCard()
                     .padding(.top, 16)
-                Text("Estimate calculated based on your showTermsspending history and available offers from eligible retailers.")
+                Text("Estimate calculated based on your spending history and available offers from eligible retailers.")
                     .font(Rewards.theme.fontMedium(size: 14))
                     .foregroundColor(Rewards.theme.secondaryTextColor)
                     .padding(.top, 16)

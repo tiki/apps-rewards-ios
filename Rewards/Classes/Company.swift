8b346760f4d7634d927db0063f9d6e9f17d16e39
Jessé Monteiro <jessemonteiroferreira@gmail.com>
2023-12-19 17:14:33 -0300

    fix company

--- Rewards/Classes/Company.swift
+++ Rewards/Classes/Company.swift
@@ -6,15 +6,8 @@
 import Foundation
 
 public struct Company {
-    var name: String
-    var jurisdiction: String
-    var privacy: String
-    var terms: String
-//    
-//    public init(name: String, jurisdiction: String, privacy: String, terms: String) {
-//        self.name = name
-//        self.jurisdiction = jurisdiction
-//        self.privacy = privacy
-//        self.terms = terms
-//    }
+    var name: String = "Company Inc."
+    var jurisdiction: String = "Tennessee, USA"
+    var privacy: String = "https://your-co.com/privacy"
+    var terms: String = "https://your-co.com/terms"
 }
